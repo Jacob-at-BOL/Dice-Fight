0f7abcf728894b5df7e82039ce0c003eafed618d
Jake Simpson <102685368+Jacob-at-BOL@users.noreply.github.com>
2024-07-15 13:35:36 -0400

    Initial Commit

---
+++ Dice Fight/Dice_FightApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dice_FightApp.swift
+//  Dice Fight
+//
+//  Created by Jacob on 7/15/24.
+//
+
+import SwiftUI
+
+@main
+struct Dice_FightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
