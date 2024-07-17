016d4c0b7bac409fb4ef0129c48ef06e98491ef2
Jake Simpson <102685368+Jacob-at-BOL@users.noreply.github.com>
2024-07-17 19:39:06 -0400

    First Real Push
    Just trying to get setup on Desktop

---
+++ Dice Fight/Launch Screen.swift
@@ -0,0 +1,18 @@
+//
+//  Launch Screen.swift
+//  Dice Fight
+//
+//  Created by Jacob on 7/15/24.
+//
+
+import SwiftUI
+
+struct Launch_Screen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Launch_Screen()
+}
